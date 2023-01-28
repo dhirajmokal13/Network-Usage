9542d8bd7537d6f96fb5bf29a2af068be1d4078f
Dhiraj <dhirajmokal13@gmail.com>
2023-01-28 22:27:35 +0530

    first version of network coverage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { MapContainer, TileLayer, Popup, Circle } from 'react-leaflet';
+import "leaflet/dist/leaflet.css";
+import Coverage from './Coverage.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <MapContainer center={{ lat: 21.1458, lng: 79.0882 }} zoom={5}>
+          <TileLayer
+            url='https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=VQ7cKnFLBfkQ4RMUGDmT'
+            attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+          />
+          {
+            Coverage && Coverage.map((item, index) => {
+              let color;
+              if (item.data >= 900) { color = '#0e0ee3' } else if (item.data >= 600) { color = '#6969eb' } else if (item.data >= 450) { color = '#a6a6ed' } else if (item.data >= 300) { color = '#a2e0fa' } else { color = '#e8b5b5' }
+              return (
+                //<Pane style={{ zIndex: 100 }} key={index}>
+                <Circle key={index} center={[item.coordinates[0], item.coordinates[1]]} radius={item.radius} pathOptions={{ color: color }} >
+                  <Popup>
+                    <span className="text-secondary d-block">City: {item.name}</span><br />
+                    <span className="text-secondary d-block">Network Usage: {item.data}</span><br />
+                    <span className="text-secondary d-block">Area: {item.radius}</span>
+                  </Popup>
+                </Circle>
+                // </Pane>
+              )
+            })
+          }
+        </MapContainer>
+      </div>
+    </>
   );
 }
 
---
+++ src/Coverage.js
@@ -0,0 +1,83 @@
+
+const Coverage = [
+    {
+        "name": "Mumbai, Maharashtra",
+        "coordinates": [19.114754, 72.882536],
+        "data": 970,
+        "radius": 36000
+    },
+    {
+        "name": "Pune, Maharashtra",
+        "coordinates": [18.527540, 73.855849],
+        "data": 890,
+        "radius": 20000
+    },
+    {
+        "name": "Delhi",
+        "coordinates": [28.704298, 77.102469],
+        "data": 910,
+        "radius": 19000
+    },
+    {
+        "name": "Hyderabad, Telangana",
+        "coordinates": [17.370691, 78.482192],
+        "data": 940,
+        "radius": 20000
+    },
+    {
+        "name": "Nagpur, Maharashtra",
+        "coordinates": [21.145702, 79.067358],
+        "data": 800,
+        "radius": 15000
+    },
+    {
+        "name": "Nashik, Maharashtra",
+        "coordinates": [19.987078, 73.797642],
+        "data": 870,
+        "radius": 16000
+    },
+    {
+        "name": "Ahmednagar, Maharashtra",
+        "coordinates": [19.104145, 74.745761],
+        "data": 880,
+        "radius": 10000
+    },
+    {
+        "name": "Amaravati, Andhra Pradesh",
+        "coordinates": [16.503139, 80.526149],
+        "data": 790,
+        "radius": 16000
+    }, {
+        "name": "Jaipur, Rajasthan",
+        "coordinates": [26.893715, 75.785054],
+        "data": 700,
+        "radius": 13000
+    }, {
+        "name": "Kota, Rajasthan",
+        "coordinates": [25.150391, 75.861366],
+        "data": 798,
+        "radius": 14000
+    }, {
+        "name": "Bengaluru, Karnataka",
+        "coordinates": [13.000798, 77.584923],
+        "data": 900,
+        "radius": 19000
+    }, {
+        "name": "Chennai, Tamil Nadu",
+        "coordinates": [13.021018, 80.194274],
+        "data": 590,
+        "radius": 19000
+    },{
+        "name": "Ranchi, Jharkhand",
+        "coordinates": [23.433150, 85.237929],
+        "data": 440,
+        "radius": 19000
+    },{
+        "name": "Ayodhya, Uttar Pradesh",
+        "coordinates": [26.790694, 82.204833],
+        "data": 290,
+        "radius": 9000
+    }
+]
+
+export default Coverage;
