05c163a354d85fb9a79193156fbb0f0c1ed94c76
Dhiraj <dhirajmokal13@gmail.com>
2023-01-29 10:57:23 +0530

    Add some Changes Regarding Feature and Ui 29 Jan

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Coverage from './Coverage.js';
 function App() {
   return (
     <>
+    <h3 className='webHead'>City Wise availablity Map ({Coverage.length} Cities Data Available)</h3>
       <div className="container">
         <MapContainer center={{ lat: 21.1458, lng: 79.0882 }} zoom={5}>
           <TileLayer
@@ -20,9 +21,9 @@ function App() {
                 //<Pane style={{ zIndex: 100 }} key={index}>
                 <Circle key={index} center={[item.coordinates[0], item.coordinates[1]]} radius={item.radius} pathOptions={{ color: color }} >
                   <Popup>
-                    <span className="text-secondary d-block">City: {item.name}</span><br />
-                    <span className="text-secondary d-block">Network Usage: {item.data}</span><br />
-                    <span className="text-secondary d-block">Area: {item.radius}</span>
+                    <span className="text-secondary d-block"><span className="city-name">City: </span>{item.name}</span><br />
+                    <span className="text-secondary d-block"><span className="city-name">Network Availablity: </span>{item.data}</span><br />
+                    <span className="text-secondary d-block"><span className="city-name">Area: </span>{item.radius} Km<sup>2</sup></span>
                   </Popup>
                 </Circle>
                 // </Pane>
@@ -30,6 +31,14 @@ function App() {
             })
           }
         </MapContainer>
+        <div style={{ marginTop: '0.5vh',marginBottom: '1vh' }}>
+          <h5 className='barHead'>City Wise Network available</h5>
+          <div className="bar" style={{ width: '10%', backgroundColor: '#e8b5b5', color: 'white'  }}>Availablity: 0 - 299</div>
+          <div className="bar" style={{ width: '20%', backgroundColor: '#a2e0fa', color: 'white'  }}>Availablity: 300 - 449</div>
+          <div className="bar" style={{ width: '30%' , backgroundColor: '#a6a6ed', color: 'white' }}>Availablity: 450 - 599</div>
+          <div className="bar" style={{ width: '40%', backgroundColor: '#6969eb', color: 'white' }}>Availablity: 600 - 899</div>
+          <div className="bar" style={{ width: '50%', backgroundColor: '#0e0ee3', color: 'white' }}>Availablity: 900 - 1000</div>
+        </div>
       </div>
     </>
   );
--- src/Coverage.js
+++ src/Coverage.js
@@ -1,82 +1,155 @@
 
 const Coverage = [
     {
-        "name": "Mumbai, Maharashtra",
+        "name": "Mumbai, Maharashtra, India",
         "coordinates": [19.114754, 72.882536],
         "data": 970,
         "radius": 36000
     },
     {
-        "name": "Pune, Maharashtra",
+        "name": "Pune, Maharashtra, India",
         "coordinates": [18.527540, 73.855849],
         "data": 890,
         "radius": 20000
     },
     {
-        "name": "Delhi",
+        "name": "Delhi, India",
         "coordinates": [28.704298, 77.102469],
         "data": 910,
         "radius": 19000
     },
     {
-        "name": "Hyderabad, Telangana",
+        "name": "Hyderabad, Telangana, India",
         "coordinates": [17.370691, 78.482192],
         "data": 940,
         "radius": 20000
     },
     {
-        "name": "Nagpur, Maharashtra",
+        "name": "Nagpur, Maharashtra, India",
         "coordinates": [21.145702, 79.067358],
         "data": 800,
         "radius": 15000
     },
     {
-        "name": "Nashik, Maharashtra",
+        "name": "Nashik, Maharashtra, India",
         "coordinates": [19.987078, 73.797642],
         "data": 870,
         "radius": 16000
     },
     {
-        "name": "Ahmednagar, Maharashtra",
+        "name": "Ahmednagar, Maharashtra, India",
         "coordinates": [19.104145, 74.745761],
         "data": 880,
         "radius": 10000
     },
     {
-        "name": "Amaravati, Andhra Pradesh",
+        "name": "Amaravati, Andhra Pradesh, India",
         "coordinates": [16.503139, 80.526149],
         "data": 790,
         "radius": 16000
     }, {
-        "name": "Jaipur, Rajasthan",
+        "name": "Jaipur, Rajasthan, India",
         "coordinates": [26.893715, 75.785054],
         "data": 700,
         "radius": 13000
     }, {
-        "name": "Kota, Rajasthan",
+        "name": "Kota, Rajasthan, India",
         "coordinates": [25.150391, 75.861366],
         "data": 798,
         "radius": 14000
     }, {
-        "name": "Bengaluru, Karnataka",
+        "name": "Bengaluru, Karnataka, India",
         "coordinates": [13.000798, 77.584923],
         "data": 900,
         "radius": 19000
     }, {
-        "name": "Chennai, Tamil Nadu",
+        "name": "Chennai, Tamil Nadu, India",
         "coordinates": [13.021018, 80.194274],
         "data": 590,
         "radius": 19000
-    },{
-        "name": "Ranchi, Jharkhand",
+    }, {
+        "name": "Ranchi, Jharkhand, India",
         "coordinates": [23.433150, 85.237929],
         "data": 440,
         "radius": 19000
-    },{
-        "name": "Ayodhya, Uttar Pradesh",
+    }, {
+        "name": "Ayodhya, Uttar Pradesh, India",
         "coordinates": [26.790694, 82.204833],
         "data": 290,
         "radius": 9000
+    }, {
+        "name": "Kanpur, Uttar Pradesh, India",
+        "coordinates": [26.451767, 80.313318],
+        "data": 299,
+        "radius": 10863
+    }, {
+        "name": "Varanasi, Uttar Pradesh, India",
+        "coordinates": [25.316401, 82.990076],
+        "data": 250,
+        "radius": 82
+    }, {
+        "name": "Jodhpur, Rajasthan, India",
+        "coordinates": [26.277423, 73.041870],
+        "data": 893,
+        "radius": 22850
+    }, {
+        "name": "Ahmedabad, Gujarat, India",
+        "coordinates": [23.038543, 72.570517],
+        "data": 912,
+        "radius": 505
+    }, {
+        "name": "Colombo, Sri Lanka",
+        "coordinates": [6.921294, 79.863840],
+        "data": 600,
+        "radius": 100
+    }, {
+        "name": "Lahore, Punjab, Pakistan",
+        "coordinates": [31.492004, 74.361815],
+        "data": 650,
+        "radius": 1772
+    }, {
+        "name": "Beijing, China",
+        "coordinates": [39.904200, 116.407396],
+        "data": 800,
+        "radius": 2200
+    },
+    {
+        "name": "Tokyo, Japan",
+        "coordinates": [35.689500, 139.691711],
+        "data": 900,
+        "radius": 3000
+    },
+    {
+        "name": "Seoul, South Korea",
+        "coordinates": [37.566535, 126.977969],
+        "data": 700,
+        "radius": 1800
+    }, {
+        "name": "Jakarta, Indonesia",
+        "coordinates": [-6.175110, 106.865039],
+        "data": 600,
+        "radius": 2000
+    }, {
+        "name": "Shanghai, China",
+        "coordinates": [31.230390, 121.473701],
+        "data": 700,
+        "radius": 2100
+    }, {
+        "name": "Manila, Philippines",
+        "coordinates": [14.5995, 120.9842],
+        "data": 500,
+        "radius": 1500
+    }, {
+        "name": "Bangkok, Thailand",
+        "coordinates": [13.7563, 100.5018],
+        "data": 400,
+        "radius": 1400
+    },
+    {
+        "name": "Kuala Lumpur, Malaysia",
+        "coordinates": [3.139003, 101.686855],
+        "data": 300,
+        "radius": 1300
     }
 ]
 
